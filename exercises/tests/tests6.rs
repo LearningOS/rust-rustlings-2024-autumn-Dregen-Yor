b7723c4528d579b0a81b264b073e2d5e466479d2
Dregen_Yor <hgcatqdao@outlook.com>
2024-10-16 10:33:30 +0800

    update

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -4,15 +4,13 @@
 // unsafe functions. Fix all the question marks and todos to make the test
 // pass.
 //
-// Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
 
-struct Foo {
+struct Foo  {
     a: u128,
     b: Option<String>,
 }
+
 
 /// # Safety
 ///
@@ -20,8 +18,10 @@ struct Foo {
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
     // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
     // simply reconstruct the box from that pointer.
-    let mut ret: Box<Foo> = unsafe { ??? };
-    todo!("The rest of the code goes here")
+    let mut ret: Box<Foo> = Box::from_raw(ptr);
+    // Modify the `b` field to be `Some("hello".to_owned())`.
+    ret.b = Some("hello".to_owned());
+    ret
 }
 
 #[cfg(test)]
